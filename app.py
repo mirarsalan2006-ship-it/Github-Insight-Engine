96d319dc7d61c525354d4b1c017db6178de45ec2
Arsalan Mir <mirarsalan2006@gmail.com>
2026-05-17 11:27:20 +0530

    updated backend logic

--- app.py
+++ app.py
@@ -398,4 +398,5 @@ def analyze_user():
         return jsonify({"error": "An unexpected server error occurred while processing data."}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
